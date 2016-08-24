bb6814b23a146c4709ca66eddcba7499e1649135
Fabio Anderegg <fabio.anderegg@liip.ch>
2016-08-24 16:20:12 +0200

    moved package to database_email

--- database_email/models.py
+++ database_email/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class EMail(models.Model):
     subject = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
